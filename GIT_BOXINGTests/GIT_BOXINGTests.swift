742bee77cefbaeda8a2670c796235bf8a1f35d80
Dariusz <dariusz@dmt-invest.pl>
2025-02-28 16:19:51 +0100

    Initial Commit

---
+++ GIT_BOXINGTests/GIT_BOXINGTests.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_BOXINGTests.swift
+//  GIT_BOXINGTests
+//
+//  Created by Dariusz Kuczynski on 28/02/2025.
+//
+
+import Testing
+@testable import GIT_BOXING
+
+struct GIT_BOXINGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
